8627481a8a451c221f2083bf1a97d07bf5346f6b
yuliangyan <y.yan@se18.qmul.ac.uk>
2019-03-28 13:28:46 +0000

    first commit!

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, request
+from cassandra.cluster import Cluster
+import requests
+
+cluster = Cluster(['cassandra'])
+session = cluster.connect()
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    name = request.args.get("name","World")
+    return('<h1>Hello, {}!</h1>'.format(name))
+
+@app.route('/predict', methods=['GET'])
+def gett():
+    air_url_template ='https://api.breezometer.com/pollen/v2/forecast/daily?lat=30.003362&lon=-95.594209&days=3&key={YOUR_API_KEY}'
+    KEY = '918f06be8ff145e6bcd4422faea391c4'
+    air_url = air_url_template.format(YOUR_API_KEY = KEY)
+    resp = requests.get(air_url)
+
+    if resp.ok:
+        cc_url = resp.json()
+    else:
+        print(resp.reason)
+    
+    categories = {categ["date"]:categ["types"]["tree"]["index"]["value"] for categ in cc_url["data"]}
+    
+    for i in categories:
+        rows = session.execute( """insert into air.stats(date,value) values('{}',{})""".format(i,categories[i]))
+    return('<h1>Predict values have been created.</h1>')
+
+@app.route('/predict/tree/<date>')
+def profile(date):
+    rows = session.execute( """select * from air.stats where date = '{}' allow filtering""".format(date))
+    for k in rows:
+        return('<h1>The forecast pollen condition value of {} is {}!</h1>'.format(date,k.value))
+    return('<h1>Not included!</h1>')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080,debug = True)
